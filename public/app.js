f8152231d7a6a37b830337af2911a559bb446d8a
Vivien Galuchot <vivien.galuchot@gmail.com>
2022-07-14 11:33:14 +0200

    improve

--- public/app.js
+++ public/app.js
@@ -9,7 +9,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import * as Puzzle from './lib/puzzle.js';
-function execWithFormData(formData, output) {
+function checkNonNull(element) {
+    if (element == null) {
+        throw new Error("missing element");
+    }
+    return element;
+}
+function checkBtn(element) {
+    if (element == null) {
+        throw new Error("missing element");
+    }
+    if (!(element instanceof HTMLButtonElement)) {
+        throw new Error("wrong element type");
+    }
+    return element;
+}
+function execWithFormData(formData, output, cancelBtn) {
     return __awaiter(this, void 0, void 0, function* () {
         function getIntProp(name) {
             if (!formData.has(name)) {
@@ -30,67 +45,66 @@ function execWithFormData(formData, output) {
         while (output.firstChild != null) {
             output.firstChild.remove();
         }
-        let cancel = document.createElement("button");
-        cancel.classList.add("cancel-btn");
-        cancel.innerText = "Cancel";
-        output.appendChild(cancel);
         let count = 0;
         let info = document.createElement("div");
         info.classList.add("info");
         output.appendChild(info);
-        info.innerText = `${count} solution found for ${rows} ${cols} ${fragments}`;
+        info.innerText = `starting ...`;
         let worker = new Worker("worker.js", { type: "module" });
         let promise = new Promise((resolve, reject) => {
-            cancel.onclick = () => {
+            cancelBtn.onclick = () => {
                 resolve(null);
             };
             worker.onmessage = (event) => {
                 count++;
-                info.innerText = `${count} solution found for ${rows} ${cols} ${fragments}`;
+                info.innerText = `${count} solution found for ${rows} ${cols} ${fragments} ...`;
                 let data = event.data;
                 if (data == null) {
                     resolve(null);
                 }
                 else {
                     let sol = Puzzle.Solution.fromObj(data);
-                    console.log(sol);
                     output.appendChild(sol.render());
                 }
             };
+            worker.onerror = (event) => {
+                console.error("Worker error", event);
+                reject(event);
+            };
         }).then(() => {
-            cancel.disabled = true;
+            cancelBtn.disabled = true;
             worker.terminate();
+            info.innerText = `${count} solution found for ${rows} ${cols} ${fragments}`;
         });
+        cancelBtn.disabled = false;
         worker.postMessage({ rows, cols, fragments });
         return promise;
     });
 }
-function formSubmit(event) {
+function formSubmit(el, runBtn) {
     return __awaiter(this, void 0, void 0, function* () {
         let toEnable = new Set();
         console.info("run...");
         try {
-            let output = document.querySelector(".gen-output");
-            if (output == null) {
-                throw new Error(`output element not found`);
-            }
+            runBtn.disabled = true;
+            let output = checkNonNull(document.querySelector(".gen-output"));
             while (output.firstChild != null) {
                 output.firstChild.remove();
             }
-            let el = event.target;
             if (!(el instanceof HTMLFormElement)) {
                 throw new Error(`element is not a form ${el}`);
             }
             let data = new FormData(el);
             for (let child of el.querySelectorAll("button, input")) {
-                if (child instanceof HTMLButtonElement || child instanceof HTMLInputElement) {
+                if (child instanceof HTMLInputElement) {
                     if (!child.disabled) {
                         toEnable.add(child);
                         child.disabled = true;
                     }
                 }
             }
-            yield execWithFormData(data, output);
+            let cancel = checkBtn(document.getElementById("btn-cancel"));
+            yield execWithFormData(data, output, cancel);
             console.info("done");
         }
         catch (error) {
@@ -100,11 +114,14 @@ function formSubmit(event) {
             for (let el of toEnable) {
                 el.disabled = false;
             }
+            runBtn.disabled = false;
         }
-        return false;
     });
 }
 // bind
-for (let el of document.getElementsByClassName("gen-form")) {
-    el.addEventListener('submit', formSubmit);
-}
+const runBtn = checkBtn(document.getElementById("btn-run"));
+runBtn.onclick = () => {
+    for (let el of document.getElementsByClassName("gen-form")) {
+        formSubmit(el, runBtn);
+    }
+};
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,24 @@
 
 import * as Puzzle from './lib/puzzle.js';
 
-async function execWithFormData(formData: FormData, output: Element) {
+function checkNonNull(element: HTMLElement | null): HTMLElement {
+    if (element == null) {
+        throw new Error("missing element");
+    }
+    return element;
+}
+
+function checkBtn(element: HTMLElement | null): HTMLButtonElement {
+    if (element == null) {
+        throw new Error("missing element");
+    }
+    if (!(element instanceof HTMLButtonElement)) {
+        throw new Error("wrong element type");
+    }
+    return element;
+}
+
+async function execWithFormData(formData: FormData, output: Element, cancelBtn: HTMLButtonElement) {
     function getIntProp(name: string): number {
         if (!formData.has(name)) {
             throw new Error(`form property ${name} missing`);
@@ -25,70 +42,68 @@ async function execWithFormData(formData: FormData, output: Element) {
         output.firstChild.remove();
     }
 
-    let cancel = document.createElement("button");
-    cancel.classList.add("cancel-btn");
-    cancel.innerText = "Cancel";
-    output.appendChild(cancel);
-
     let count = 0;
     let info = document.createElement("div");
     info.classList.add("info");
     output.appendChild(info);
-    info.innerText = `${count} solution found for ${rows} ${cols} ${fragments}`;
+    info.innerText = `starting ...`;
 
     let worker = new Worker("worker.js", { type: "module" });
     let promise = new Promise((resolve, reject) => {
-        cancel.onclick = () => {
+        cancelBtn.onclick = () => {
             resolve(null);
         };
         worker.onmessage = (event) => {
             count++;
-            info.innerText = `${count} solution found for ${rows} ${cols} ${fragments}`;
+            info.innerText = `${count} solution found for ${rows} ${cols} ${fragments} ...`;
             let data = event.data;
             if (data == null) {
                 resolve(null);
             } else {
                 let sol = Puzzle.Solution.fromObj(data);
-                console.log(sol);
                 output.appendChild(sol.render());
             }
         };
+        worker.onerror = (event) => {
+            console.error("Worker error", event);
+            reject(event);
+        };
     }).then(() => {
-        cancel.disabled = true;
+        cancelBtn.disabled = true;
         worker.terminate();
+        info.innerText = `${count} solution found for ${rows} ${cols} ${fragments}`;
     });
+    cancelBtn.disabled = false;
 
     worker.postMessage({ rows, cols, fragments });
     return promise;
 }
 
 
-async function formSubmit(event: Event) {
+async function formSubmit(el: Element, runBtn: HTMLButtonElement) {
     let toEnable = new Set<HTMLButtonElement | HTMLInputElement>();
     console.info("run...");
     try {
-        let output = document.querySelector(".gen-output");
-        if (output == null) {
-            throw new Error(`output element not found`);
-        }
+        runBtn.disabled = true;
+        let output = checkNonNull(document.querySelector(".gen-output"));
         while (output.firstChild != null) {
             output.firstChild.remove();
         }
 
-        let el = event.target;
         if (!(el instanceof HTMLFormElement)) {
             throw new Error(`element is not a form ${el}`);
         }
         let data = new FormData(el);
         for (let child of el.querySelectorAll("button, input")) {
-            if (child instanceof HTMLButtonElement || child instanceof HTMLInputElement) {
+            if (child instanceof HTMLInputElement) {
                 if (!child.disabled) {
                     toEnable.add(child);
                     child.disabled = true;
                 }
             }
         }
-        await execWithFormData(data, output);
+        let cancel = checkBtn(document.getElementById("btn-cancel"));
+        await execWithFormData(data, output, cancel);
         console.info("done");
     } catch (error) {
         console.error("execution failed", error);
@@ -96,13 +111,16 @@ async function formSubmit(event: Event) {
         for (let el of toEnable) {
             el.disabled = false;
         }
+        runBtn.disabled = false;
     }
-    return false;
 }
 
 
 // bind
 
-for (let el of document.getElementsByClassName("gen-form")) {
-    el.addEventListener('submit', formSubmit);
-}
+const runBtn = checkBtn(document.getElementById("btn-run"));
+runBtn.onclick = () => {
+    for (let el of document.getElementsByClassName("gen-form")) {
+        formSubmit(el, runBtn);
+    }
+};
